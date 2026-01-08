dd59f0885ab00ef9676fe35e06ec2198cf809d4d
Dhrumisha Rakholiya <dhrumisharakholiya12@gmail.com>
2026-01-08 23:33:04 +0530

    get Me APi fix

--- frontend/src/services/auth/auth.service.tsx
+++ frontend/src/services/auth/auth.service.tsx
@@ -96,7 +96,7 @@ export const getMe = async (): Promise<MeResponse> => {
     const { data } = await axiosInstance.get<MeResponse>("/user/me");
     return data;
   } catch (error: any) {
-    throw toast.error(error?.response?.data?.message || "Unauthorized");
+    throw console.error(error?.response?.data?.message || "Unauthorized");
   }
 };
 
