251512d175325839daf185528d95854cb809627f
Dhrumisha Rakholiya <dhrumisharakholiya12@gmail.com>
2026-01-08 23:33:04 +0530

    get Me APi fix

--- frontend/src/app/(auth)/sign-in/page.tsx
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -1,10 +1,12 @@
 import SignIn from "@/admin-pages/auth/SignIn";
+import { AuthInitializer } from "@/components/AuthInitializer";
 import React from "react";
 
 const SignInPage = () => {
   return (
     <>
     <SignIn />
+    <AuthInitializer />
     </>
   );
 };
