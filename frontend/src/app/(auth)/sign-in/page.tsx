ada2b38a1cccdae4d91fce6cf80e98d320160c9f
Dhrumisha Rakholiya <dhrumisharakholiya12@gmail.com>
2026-01-08 23:47:40 +0530

    fix redux

--- frontend/src/app/(auth)/sign-in/page.tsx
+++ frontend/src/app/(auth)/sign-in/page.tsx
@@ -1,12 +1,10 @@
 import SignIn from "@/admin-pages/auth/SignIn";
-import { AuthInitializer } from "@/components/AuthInitializer";
 import React from "react";
 
 const SignInPage = () => {
   return (
     <>
     <SignIn />
-    <AuthInitializer />
     </>
   );
 };
